7aeab42ba3bb8db40c4a63f3fc25d3dade869ab7
Yari-Carelli <yaricarelli@hotmail.it>
2022-05-04 12:26:53 +0000

    Add function to encrypt the string according to the morse code chart

--- run.py
+++ run.py
@@ -26,3 +26,21 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
 					'?':'..--..', '/':'-..-.', '-':'-....-',
 					'(':'-.--.', ')':'-.--.-'}
 
+# Function to encrypt the string
+# according to the morse code chart
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+ 
+            # Looks up the dictionary and adds the
+            # corresponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+ 
+    return cipher
