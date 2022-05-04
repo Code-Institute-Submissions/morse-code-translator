f34f638c17303d12fef824b20fcda3fdbeac8570
Yari-Carelli <yaricarelli@hotmail.it>
2022-05-04 12:25:37 +0000

    Add dictionary representing the morse code chart

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Python program to implement Morse Code Translator
+
+'''
+VARIABLE KEY
+'cipher' -> 'stores the morse translated form of the english string'
+'decipher' -> 'stores the english translated form of the morse string'
+'citext' -> 'stores morse code of a single character'
+'i' -> 'keeps count of the spaces between morse characters'
+'message' -> 'stores the string to be encoded or decoded'
+'''
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
+
